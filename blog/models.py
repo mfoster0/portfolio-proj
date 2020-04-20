cb084713fb36619f74f2091deb8e8849a34ce0a3
mark foster <mark.foster@live.co.uk>
2020-04-20 13:19:13 +0100

    Added bootstrap and connected jobs to postgres db

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Blog(models.Model):
+    title = models.CharField(max_length=100)
+    pubdate = models.DateTimeField()
+    body = models.CharField(max_length=2000)
+    image = models.ImageField(upload_to='blogimages/') #other params available like size
--- jobs/views.py
+++ jobs/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Job
 
 # Create your views here.
+def home(request):
+    jobs = Job.objects #retrieve the jobs from the database
+    return render(request, 'jobs/home.html', {"jobs":jobs})

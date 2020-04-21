6aa24ff9877c955b790a2fd2a0a2b154752d01fb
mark foster <mark.foster@live.co.uk>
2020-04-21 16:40:29 +0100

    replaced personal images

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'portfoliodb',
         'USER': 'postgres',
-        'PASSWORD': 'testthis',
+        'PASSWORD': 'somepwd',
         'HOST':'localhost',
         'PORT': '5432',
         
